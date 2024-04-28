b05aa3fd452ddcbfbd65e5036b03811197f9441a
David Feldman <djfeld01@gmail.com>
2024-04-28 08:49:31 -0400

    cleaned up navigation, added outline for routing options

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -70,23 +70,23 @@ export default function Main (){
       <DollarSign className="h-4 w-4 text-muted-foreground" />
     </CardHeader>
     <CardContent>
-      <div className="text-2xl font-bold">Hi Jacque, how are you</div>
+      <div className="text-2xl font-bold">This can be an Event Listing</div>
       <p className="text-xs text-muted-foreground">
-        +20.1% from last month
+        Like wrestling or something
       </p>
     </CardContent>
   </Card>
   <Card x-chunk="dashboard-01-chunk-1">
     <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
       <CardTitle className="text-sm font-medium">
-        Subscriptions
+        Something will go here too
       </CardTitle>
       <Users className="h-4 w-4 text-muted-foreground" />
     </CardHeader>
     <CardContent>
       <div className="text-2xl font-bold">+2350</div>
       <p className="text-xs text-muted-foreground">
-        +180.1% from last month
+        I dunno
       </p>
     </CardContent>
   </Card>
