f741471bc33bfbab5f229e989cfc4747aae2a710
David Feldman <djfeld01@gmail.com>
2024-04-29 21:34:26 -0400

    cleaned up event page and added a loop

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -17,202 +17,63 @@ import {
   TableBody,
   TableCell,
 } from "@/components/ui/table";
+import { Breadcrumb } from "@/components/ui/breadcrumb";
 
 const competitors = [
-  { name: "wrestler1" },
-  { name: "wrestler1" },
-  { name: "wrestler1" },
-  { name: "wrestler1" },
+  { name: "wrestler1", school: "penn state" },
+  { name: "wrestler2", school: "Ohio State" },
+  { name: "wrestler3", school: "Penn State Again" },
+  { name: "wrestler4", school: "Iowa" },
 ];
 
 export default function Competitions() {
   return (
-    <div className="grid gap-4 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
-      <Card className="xl:col-span-2" x-chunk="dashboard-01-chunk-4">
-        <CardHeader className="flex flex-row items-center">
-          <div className="grid gap-2">
-            <CardTitle>Event</CardTitle>
-            <CardDescription>2024 NCAA Wrestling 125</CardDescription>
-          </div>
-        </CardHeader>
-        <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Seed</TableHead>
-                <TableHead>Odds</TableHead>
-                <TableHead>Wrestler</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell>
-                  <div className="font-medium">Braeden Davis</div>
-                  <div className="hidden text-sm text-muted-foreground md:inline">
-                    Penn State
-                  </div>
-                </TableCell>
-                <TableCell>Sale</TableCell>
-                <TableCell>
-                  <Badge className="text-xs" variant="outline">
-                    Approved
-                  </Badge>
-                </TableCell>
-                <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                  2023-06-23
-                </TableCell>
-                <TableCell className="text-right">$250.00</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>
-                  <div className="font-medium">Olivia Smith</div>
-                  <div className="hidden text-sm text-muted-foreground md:inline">
-                    olivia@example.com
-                  </div>
-                </TableCell>
-                <TableCell className="hidden xl:table-column">Refund</TableCell>
-                <TableCell className="hidden xl:table-column">
-                  <Badge className="text-xs" variant="outline">
-                    Declined
-                  </Badge>
-                </TableCell>
-                <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                  2023-06-24
-                </TableCell>
-                <TableCell className="text-right">$150.00</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>
-                  <div className="font-medium">Noah Williams</div>
-                  <div className="hidden text-sm text-muted-foreground md:inline">
-                    noah@example.com
-                  </div>
-                </TableCell>
-                <TableCell className="hidden xl:table-column">
-                  Subscription
-                </TableCell>
-                <TableCell className="hidden xl:table-column">
-                  <Badge className="text-xs" variant="outline">
-                    Approved
-                  </Badge>
-                </TableCell>
-                <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                  2023-06-25
-                </TableCell>
-                <TableCell className="text-right">$350.00</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>
-                  <div className="font-medium">Emma Brown</div>
-                  <div className="hidden text-sm text-muted-foreground md:inline">
-                    emma@example.com
-                  </div>
-                </TableCell>
-                <TableCell className="hidden xl:table-column">Sale</TableCell>
-                <TableCell className="hidden xl:table-column">
-                  <Badge className="text-xs" variant="outline">
-                    Approved
-                  </Badge>
-                </TableCell>
-                <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                  2023-06-26
-                </TableCell>
-                <TableCell className="text-right">$450.00</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>
-                  <div className="font-medium">Liam Johnson</div>
-                  <div className="hidden text-sm text-muted-foreground md:inline">
-                    liam@example.com
-                  </div>
-                </TableCell>
-                <TableCell className="hidden xl:table-column">Sale</TableCell>
-                <TableCell className="hidden xl:table-column">
-                  <Badge className="text-xs" variant="outline">
-                    Approved
-                  </Badge>
-                </TableCell>
-                <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
-                  2023-06-27
-                </TableCell>
-                <TableCell className="text-right">$550.00</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </CardContent>
-      </Card>
-      <Card x-chunk="dashboard-01-chunk-5">
-        <CardHeader>
-          <CardTitle>Place Bet</CardTitle>
-        </CardHeader>
-        <CardContent className="grid gap-8">
-          <div className="flex items-center gap-4">
-            <Avatar className="hidden h-9 w-9 sm:flex">
-              <AvatarImage src="/avatars/01.png" alt="Avatar" />
-              <AvatarFallback>OM</AvatarFallback>
-            </Avatar>
-            <div className="grid gap-1">
-              <p className="text-sm font-medium leading-none">Olivia Martin</p>
-              <p className="text-sm text-muted-foreground">
-                olivia.martin@email.com
-              </p>
+    <div>
+      <div className="grid gap-4 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+        <Card className="lg:col-span-2" x-chunk="dashboard-01-chunk-4">
+          <CardHeader className="flex flex-row items-center">
+            <div className="grid gap-2">
+              <CardTitle>Event</CardTitle>
+              <CardDescription>2024 NCAA Wrestling 125</CardDescription>
             </div>
-            <div className="ml-auto font-medium">+$1,999.00</div>
-          </div>
-          <div className="flex items-center gap-4">
-            <Avatar className="hidden h-9 w-9 sm:flex">
-              <AvatarImage src="/avatars/02.png" alt="Avatar" />
-              <AvatarFallback>JL</AvatarFallback>
-            </Avatar>
-            <div className="grid gap-1">
-              <p className="text-sm font-medium leading-none">Jackson Lee</p>
-              <p className="text-sm text-muted-foreground">
-                jackson.lee@email.com
-              </p>
-            </div>
-            <div className="ml-auto font-medium">+$39.00</div>
-          </div>
-          <div className="flex items-center gap-4">
-            <Avatar className="hidden h-9 w-9 sm:flex">
-              <AvatarImage src="/avatars/03.png" alt="Avatar" />
-              <AvatarFallback>IN</AvatarFallback>
-            </Avatar>
-            <div className="grid gap-1">
-              <p className="text-sm font-medium leading-none">
-                Isabella Nguyen
-              </p>
-              <p className="text-sm text-muted-foreground">
-                isabella.nguyen@email.com
-              </p>
-            </div>
-            <div className="ml-auto font-medium">+$299.00</div>
-          </div>
-          <div className="flex items-center gap-4">
-            <Avatar className="hidden h-9 w-9 sm:flex">
-              <AvatarImage src="/avatars/04.png" alt="Avatar" />
-              <AvatarFallback>WK</AvatarFallback>
-            </Avatar>
-            <div className="grid gap-1">
-              <p className="text-sm font-medium leading-none">William Kim</p>
-              <p className="text-sm text-muted-foreground">will@email.com</p>
-            </div>
-            <div className="ml-auto font-medium">+$99.00</div>
-          </div>
-          <div className="flex items-center gap-4">
-            <Avatar className="hidden h-9 w-9 sm:flex">
-              <AvatarImage src="/avatars/05.png" alt="Avatar" />
-              <AvatarFallback>SD</AvatarFallback>
-            </Avatar>
-            <div className="grid gap-1">
-              <p className="text-sm font-medium leading-none">Sofia Davis</p>
-              <p className="text-sm text-muted-foreground">
-                sofia.davis@email.com
-              </p>
-            </div>
-            <div className="ml-auto font-medium">+$39.00</div>
-          </div>
-        </CardContent>
-      </Card>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Seed</TableHead>
+                  <TableHead>Odds</TableHead>
+                  <TableHead>Wrestler</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {competitors.map((competitor, index) => (
+                  <TableRow key={index}>
+                    <TableCell>
+                      <div className="font-medium">1</div>
+                    </TableCell>
+                    <TableCell>
+                      <div className="font-medium">3:1</div>
+                    </TableCell>
+                    <TableCell>
+                      <div className="font-medium">{competitor.name}</div>
+                      <div className="hidden text-sm text-muted-foreground md:inline">
+                        {competitor.school}
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+        <Card x-chunk="dashboard-01-chunk-5">
+          <CardHeader>
+            <CardTitle>Place Bet</CardTitle>
+          </CardHeader>
+          <CardContent className="grid gap-8"></CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
